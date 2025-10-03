46091224a2967357b0c35e356d54d94602b51004
AwertzHVD <104345472+AwertzHVD@users.noreply.github.com>
2025-10-03 14:28:09 +0200

    Update MyFirstClass.java

--- MyFirstProject/src/MyFirstClass.java
+++ MyFirstProject/src/MyFirstClass.java
@@ -1,5 +1,56 @@
+/**
+ * author Franz Aguillon
+ * <p>
+ * First exercise
+ */
+
 public class MyFirstClass {
     static void main(String[] args) {
-        System.out.println("Hello World!");
+//        helloWorld();
+//        intAndLongVariables();
+//        arithmeticOperations();
+//        convertCentToEuro();
+    }
+
+    static void convertCentToEuro() {
+        int cent = 5120; //cent
+        Out.print(cent / 100); // 51
+        Out.print(",");
+        Out.print(cent % 100); // 20
+        Out.print(" EUR");
+        Out.print((cent / 100) + ", " + (cent % 100) + " EUR");
+    }
+
+    static void arithmeticOperations() {
+        // Output should look like this 1H 2Z 3E
+        int val = 123;
+        Out.print(val / 100);
+        Out.println("H");
+        Out.print(val / 10 % 10);
+        Out.println("Z");
+        Out.print(val % 10);
+        Out.println("E");
+    }
+
+    static void helloWorld() {
+        // System.out.println() wird durch Out.println() ersetzt
+        Out.println("Hello, World!"); // method of the inout library
+    }
+
+    static void intAndLongVariables() {
+        int a;
+        a = 10;
+        // a = 3000000000L; // compiler error, number too big
+        long l = 3000000000L; // okay
+        Out.println(a);
+    }
+
+    void readFromConsole() {
+        int a = 10;
+        int b = In.readInt(); // function of the inout library
+        int c = In.readInt(); // function of the inout library
+        int sum = a + b + c;
+        Out.print("a + b + c = : ");
+        Out.println(sum);
     }
 }
